3318394674f3ac3ff10b5dd5c16438238bb767fe
ygautomo <y_gautomo@yahoo.com>
2019-05-27 01:50:31 +0700

    Update App.js

--- app.js
+++ app.js
@@ -2,14 +2,20 @@ const express = require('express');
 const app = express();
 
 // Put your answer route here
-app.get('/', (req, res) => res.send("Hello World !!"));
+// app.get('/', (req, res) => res.send("Hello World !!"));
+app.get('/me', (req, res) => res.json({
+     "Name":"Yugo Gautomo",
+     "Email": "y_gautomo@yahoo.com",
+     "Occupation": "Refactory Student"
+ 		})
+	);
 
 app.listen(3000, () => console.log("Port 3000 initialized and ready!"));
 
 app.use(function(req, res, next) {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-  });
+		let err = new Error('Not Found');
+		err.status = 404;
+		next(err);
+	});
 
 module.exports = app;
