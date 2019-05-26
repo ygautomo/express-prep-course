54dd0ed0b4042112af0f380c574f89af8b52b79b
ygautomo <y_gautomo@yahoo.com>
2019-05-27 01:45:41 +0700

    Update App.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 // Put your answer route here
 app.get('/', (req, res) => res.send("Hello World !!"));
 
-app.listen(3000, () => console.log("Port 3000 initialized and ready!");
+app.listen(3000, () => console.log("Port 3000 initialized and ready!"));
 
 app.use(function(req, res, next) {
     let err = new Error('Not Found');
